722534fb9bbccf378802e20aa041fd38b2fcd1f4
Arturo Cuya <arturocuya099@gmail.com>
2019-06-03 10:27:39 -0500

    finished

---
+++ src/composing/split-temporary-variable_after.java
@@ -0,0 +1,4 @@
+double perimeter = 2 * (height + width);
+System.out.println(perimeter);
+double area = height * width;
+System.out.println(area);
--- src/composing/split-temporary-variable_before.java
+++
@@ -1,4 +0,0 @@
-double temp = 2 * (height + width);
-System.out.println(temp);
-temp = height * width;
-System.out.println(temp);
